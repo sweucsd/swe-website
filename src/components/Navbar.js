1d2411622be2d5ed56360643fc64adc80b5151cd
Allison Bhavsar <albhavsa@ucsd.edu>
2021-04-26 18:47:14 -0700

    made navbar fixed and added transition

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,6 +17,7 @@ import Header from './Header';
 import Footer from './Footer';
 import NotFound from '../pages/NotFound';
 import Hamburger from '../assets/hamburger.png';
+import Close from '../assets/close.png';
 
 
 function Navbar() {
@@ -32,27 +33,31 @@ function Navbar() {
 
   return (
     <div>
-      <nav className="topNav p1">
-        <NavLink exact to="/"><img className="logo" src={Logo} alt="logo" /></NavLink>
-        <NavLink exact className="link" to="/">Home</NavLink>
-        <NavLink exact className="link" to="/about">About</NavLink>
-        <NavLink exact className="link" to="/events">Events</NavLink>
-        <NavLink exact className="link" to="/involvement">Get Involved</NavLink>
-        <NavLink exact className="link" to="/outreach">Outreach</NavLink>
-        <NavLink exact className="link" to="/sponsors">Sponsors</NavLink>
-        <button className="hamburgerContainer" onClick={() => setShowSideNav(!showSideNav)}>
-          <img className="hamburger" src={Hamburger} alt='' />
-        </button>
-      </nav>
-      {showSideNav &&
-        <nav className="sideNav p1">
+      <div className="navContainer">
+        <nav className="topNav p1">
+          <NavLink exact to="/"><img className="logo" src={Logo} alt="logo" /></NavLink>
+          <NavLink exact className="link" to="/">Home</NavLink>
+          <NavLink exact className="link" to="/about">About</NavLink>
+          <NavLink exact className="link" to="/events">Events</NavLink>
+          <NavLink exact className="link" to="/involvement">Get Involved</NavLink>
+          <NavLink exact className="link" to="/outreach">Outreach</NavLink>
+          <NavLink exact className="link" to="/sponsors">Sponsors</NavLink>
+          <button className="hamburgerContainer" onClick={() => setShowSideNav(!showSideNav)}>
+            {showSideNav ?
+              <img className="closeIcon" src={Close} alt='' /> :
+              <img className="hamburger" src={Hamburger} alt='' />
+            }
+          </button>
+        </nav>
+        <nav className={`${showSideNav ? "sideNav" : "sideNavClosed"} p1`}>
           <NavLink exact className="sideLink" to="/">Home</NavLink>
           <NavLink exact className="sideLink" to="/about">About</NavLink>
           <NavLink exact className="sideLink" to="/events">Events</NavLink>
           <NavLink exact className="sideLink" to="/involvement">Get Involved</NavLink>
           <NavLink exact className="sideLink" to="/outreach">Outreach</NavLink>
           <NavLink exact className="sideLink" to="/sponsors">Sponsors</NavLink>
-        </nav>}
+        </nav>
+      </div>
 
       <Header />
 
@@ -80,7 +85,7 @@ function Navbar() {
         </Route>
       </Switch>
 
-      <Footer /> 
+      <Footer />
     </div>
   );
 }
