e1fedc8a8cfc4f97437cfb7493dfb74cd58c915d
Allison Bhavsar <albhavsa@ucsd.edu>
2021-05-07 01:28:15 -0700

    moved projects to separate page and made navbar easier to update

---
+++ src/data/Pages.js
@@ -0,0 +1,77 @@
+import Home from '../pages/Home';
+import GetInvolved from '../pages/GetInvolved';
+import Sponsors from '../pages/Sponsors';
+import Calendar from '../pages/Calendar';
+import Outreach from '../pages/Outreach';
+import About from '../pages/About';
+import Projects from '../pages/Projects';
+
+const Pages = [
+  {
+    path: '/',
+    title: 'SWE @ UCSD',
+    subtitle: 'Aspire / Advance / Achieve',
+    titleTag: 'SWE UCSD',
+    navLabel: 'Home',
+    component: Home,
+  },
+  {
+    path: '/about',
+    title: 'About Us',
+    subtitle: 'Learn about who we are and what it means to be a SWE Bee!',
+    titleTag: 'About – SWE UCSD',
+    navLabel: 'About',
+    component: About,
+  },
+  {
+    path: '/events',
+    title: 'SWE Calendar',
+    subtitle: 'Subscribe to our calendar to stay notified about our upcoming events!',
+    titleTag: 'Calendar – SWE UCSD',
+    navLabel: 'Calendar',
+    component: Calendar,
+  },
+  {
+    path: '/involvement',
+    title: 'Get Involved',
+    subtitle: 'Find out how to be active in our chapter',
+    titleTag: 'Involvement – SWE UCSD',
+    navLabel: 'Involvement',
+    component: GetInvolved,
+  },
+  {
+    path: '/projects',
+    title: 'Team Projects',
+    subtitle: 'Learn about our current projects',
+    titleTag: 'Projects – SWE UCSD',
+    navLabel: 'Projects',
+    component: Projects,
+  },
+  {
+    path: '/outreach',
+    title: 'Outreach',
+    subtitle: 'Help us inspire young women to pursue engineering',
+    titleTag: 'Outreach – SWE UCSD',
+    navLabel: 'Outreach',
+    component: Outreach,
+  },
+  {
+    path: '/professional',
+    title: 'maybe: Professional Development?',
+    subtitle: 'WE20 Conference and opportunities info page?',
+    titleTag: 'Professional – SWE UCSD',
+    navLabel: 'Professional',
+    component: Outreach,
+  },
+  {
+    path: '/sponsors',
+    title: 'Sponsors',
+    subtitle: 'Thanks to our sponsors for their continued support!',
+    titleTag: 'Sponsors – SWE UCSD',
+    navLabel: 'Sponsors',
+    component: Sponsors,
+  },
+
+];
+
+export default Pages;
--- src/pages/GetInvolved.js
+++ src/pages/GetInvolved.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import './GetInvolved.css';
-import Projects from '../data/Projects';
-import ProjectCard from '../components/ProjectCard';
 import Button from '../components/Button';
-import Gears from '../assets/swe_gears_2.png';
 
 function GetInvolved() {
   return (
@@ -88,48 +85,6 @@ function GetInvolved() {
           <p className="p2 membershipBody">
             Board election details?
           </p>
-        </div>
-      </div>
-      <div className="margin projContainer">
-        <div className="projHeader">
-          <div>
-            <h1 className="h2 projTitle">Join a Project Team</h1>
-            <h1 className="h3 projSubtitle">Gain Technical Experience</h1>
-          </div>
-          <img src={Gears} className="threeGears" alt="" />
-        </div>
-        <p className="p1 projDescription">
-          Projects are a great way to stay involved with the SWE community
-          and earn hours towards membership while gaining hands-on technical
-          experience and meeting fellow SWE Bees. Stay subscribed to our
-          newsletter and attend GBMs to hear about future project opportunities.
-          Here are some of our current projects:
-        </p>
-        {Projects.map((project, index) => {
-          if (index % 2 === 0) {
-            return (
-              <ProjectCard
-                name={project.name}
-                description={project.description}
-                right={false}
-              />
-            );
-          }
-
-          return <ProjectCard name={project.name} description={project.description} right />;
-        })}
-        <div className="sweetIdeasContainer">
-          <h1 className="h2 projTitle">SWE-et Ideas!</h1>
-          <p className="p1 sweetIdeasBody">
-            Interested in starting your own project? Fill out the
-            SWE-et Ideas project application
-            for a chance to bring your ideas to life! If your application is selected,
-            we&apos;ll help you form a team and provide you with funding
-            for your very own project.
-          </p>
-          <a target="_blank" rel="noopener noreferrer" href="https://docs.google.com/forms/d/e/1FAIpQLSfnSqAgbqSM9LhiWwLa6g5HYfKgdbCFzLfMuGGP3OcClm6hrw/viewform">
-            <Button label="Apply Here" />
-          </a>
         </div>
       </div>
     </div>
