7c64ce32c9dfb5db76ce91f35b686d98a40ffc6d
Allison Bhavsar <albhavsa@ucsd.edu>
2021-05-12 16:57:52 -0700

    moved sponsors info to separate file

---
+++ src/data/Sponsors.js
@@ -0,0 +1,41 @@
+import BoeingLogo from '../assets/sponsors/Boeing_Logo.png';
+import BrainCorpLogo from '../assets/sponsors/Brain_Corp_Logo.jpeg';
+import CamposEPCLogo from '../assets/sponsors/Campos_EPC_Logo.jpg';
+import LawrenceLivNatLab from '../assets/sponsors/Lawrence_Liv_Nat_Lab_logo.jpeg';
+import LockheedMartinLogo from '../assets/sponsors/Lockheed_Martin_Logo.jpeg';
+import NorthropGrummanLogo from '../assets/sponsors/Northrop_Grumman_Logo.svg.png';
+import Nuvasive from '../assets/sponsors/Nuvasive_logo.jpeg';
+import Splunk from '../assets/sponsors/splunk_logo.jpg';
+import TESCLogo from '../assets/sponsors/TESC_logo.png';
+
+const Sponsors = [
+  {
+    image: BoeingLogo,
+  },
+  {
+    image: BrainCorpLogo,
+  },
+  {
+    image: CamposEPCLogo,
+  },
+  {
+    image: LawrenceLivNatLab,
+  },
+  {
+    image: LockheedMartinLogo,
+  },
+  {
+    image: NorthropGrummanLogo,
+  },
+  {
+    image: Nuvasive,
+  },
+  {
+    image: Splunk,
+  },
+  {
+    image: TESCLogo,
+  },
+];
+
+export default Sponsors;
