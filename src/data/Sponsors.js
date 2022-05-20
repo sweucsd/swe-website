4cbbbde7aeb3aec8d6783879a2d8d2c21d0cb3f6
Jaslina Kochhar <60686299+jaslinakaur@users.noreply.github.com>
2022-05-19 19:09:34 -0700

    changed sponsors to current ones

--- src/data/Sponsors.js
+++ src/data/Sponsors.js
@@ -1,40 +1,28 @@
 import BoeingLogo from '../assets/sponsors/Boeing_Logo.png';
-import BrainCorpLogo from '../assets/sponsors/Brain_Corp_Logo.jpeg';
-import CamposEPCLogo from '../assets/sponsors/Campos_EPC_Logo.jpg';
-import LawrenceLivNatLab from '../assets/sponsors/Lawrence_Liv_Nat_Lab_logo.jpeg';
+import TrellisWare from '../assets/sponsors/TrellisWare.png';
+import LLNL from '../assets/sponsors/Lawrence_Liv_Nat_Lab_logo.jpeg';
+import JaneStreet from '../assets/sponsors/JaneStreet.png';
 import LockheedMartinLogo from '../assets/sponsors/Lockheed_Martin_Logo.jpeg';
-import NorthropGrummanLogo from '../assets/sponsors/Northrop_Grumman_Logo.svg.png';
-import Nuvasive from '../assets/sponsors/Nuvasive_logo.jpeg';
-import Splunk from '../assets/sponsors/splunk_logo.png';
-import TESCLogo from '../assets/sponsors/TESC_logo.png';
+import mmccorp from '../assets/sponsors/mmccorp.png';
 
 const Sponsors = [
   {
     image: BoeingLogo,
   },
   {
-    image: BrainCorpLogo,
+    image: TrellisWare,
   },
   {
-    image: CamposEPCLogo,
+    image: LLNL,
   },
   {
-    image: LawrenceLivNatLab,
+    image: JaneStreet,
   },
   {
     image: LockheedMartinLogo,
   },
   {
-    image: NorthropGrummanLogo,
-  },
-  {
-    image: Nuvasive,
-  },
-  {
-    image: Splunk,
-  },
-  {
-    image: TESCLogo,
+    image: mmccorp,
   },
 ];
 
