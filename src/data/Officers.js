ff8f203affec4f4c8b1cd54a44dc15644a1cc8fd
sweucsd <39417244+sweucsd@users.noreply.github.com>
2022-11-17 10:01:54 -0800

    updates profile images in officer.js

--- src/data/Officers.js
+++ src/data/Officers.js
@@ -2,13 +2,13 @@ import PresidentImg from '../assets/officer_images/Erin.jpg';
 import VPExternalImg from '../assets/officer_images/Elianor.jpg';
 import VPInternalImg from '../assets/officer_images/Erica.JPG';
 import TreasurerImg from '../assets/officer_images/Angkitha.jpg';
-import SecretaryImg from '../assets/officer_images/Makena.jpeg';
+import SecretaryImg from '../assets/officer_images/Samantha.png';
 import EnvisionImg from '../assets/officer_images/Tiffany.png';
 import EDGEImg from '../assets/officer_images/Marisa.jpg';
 import SWENextImg from '../assets/officer_images/Hailey.JPG';
 import TechDevImg from '../assets/officer_images/Jaslina.jpg';
-import SocialImg from '../assets/officer_images/Cate.jpg';
-import AcademicImg from '../assets/officer_images/Kira.jpg';
+import SocialImg from '../assets/officer_images/Allison.png';
+import AcademicImg from '../assets/officer_images/Harsheta.png';
 import PublicityImg from '../assets/officer_images/Josephine.jpg';
 
 const Officers = [
