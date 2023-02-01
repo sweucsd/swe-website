306090d5e7920b820e4a99b5cc533fa98f597179
sweucsd <39417244+sweucsd@users.noreply.github.com>
2023-02-01 10:56:39 -0800

    updates sponsors.js

--- src/data/Sponsors.js
+++ src/data/Sponsors.js
@@ -1,29 +1,47 @@
-import BoeingLogo from '../assets/sponsors/Boeing_Logo.png';
+import ATandT from '../assets/sponsors/at&t.png';
 import TrellisWare from '../assets/sponsors/TrellisWare.jpeg';
-import Llnl from '../assets/sponsors/Lawrence_Liv_Nat_Lab_logo.jpeg';
-import JaneStreet from '../assets/sponsors/JaneStreet.png';
+import Navy from '../assets/sponsors/navy.png';
+import Kite from '../assets/sponsors/kite.png';
 import LockheedMartinLogo from '../assets/sponsors/Lockheed_Martin_Logo.jpeg';
-import Mmccorp from '../assets/sponsors/mmccorp.png';
+import Meta from '../assets/sponsors/meta.png';
+import Edison from '../assets/sponsors/edison.png';
+import Collins from '../assets/sponsors/collins_aerospace.png';
+import Gilead from '../assets/sponsors/gilead.png';
+import Cruise from '../assets/sponsors/cruise.png';
+
 
 const Sponsors = [
   {
-    image: BoeingLogo,
+    image: ATandT,
   },
   {
     image: TrellisWare,
   },
   {
-    image: Llnl,
+    image: Navy,
   },
   {
-    image: JaneStreet,
+    image: Kite,
   },
   {
     image: LockheedMartinLogo,
   },
   {
-    image: Mmccorp,
+    image: Meta,
   },
+  {
+    image: Edison,
+  },
+  {
+    image: Collins,
+  },
+  {
+    image: Gilead,
+  },
+  {
+    image: Cruise,
+  },
+  
 ];
 
 export default Sponsors;
