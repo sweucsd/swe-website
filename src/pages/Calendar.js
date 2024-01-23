434330e6ec1c3beaa7dc103de6d9d34471500f78
sweucsd <39417244+sweucsd@users.noreply.github.com>
2024-01-23 13:49:49 -0800

    Update Calendar.js

--- src/pages/Calendar.js
+++ src/pages/Calendar.js
@@ -51,7 +51,7 @@ function Calendar() {
       <div className="margin lightPurpleBg calendarBody">
         <div className="calendarContainer">
           <div className="calendar">
-            <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=America%2FLos_Angeles&showPrint=0&src=Y190bTdmOWJodWo3ZTFzMXJ2MnRkZjhlb3Vnb0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%238E24AA" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+            <iframe title="swe-cal" src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=America%2FLos_Angeles&showPrint=0&src=Y190bTdmOWJodWo3ZTFzMXJ2MnRkZjhlb3Vnb0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%238E24AA" style="border-width:0" width="800" height="600" frameBorder="0" scrolling="no"/>
           </div>
         </div>
       </div>
