aa9e9cbb1066f41c05dd8f61827d76a40696542e
sweucsd <39417244+sweucsd@users.noreply.github.com>
2024-12-13 16:55:49 -0800

    Update Officers.js

--- src/data/Officers.js
+++ src/data/Officers.js
@@ -1,124 +1,144 @@
-import PresidentImg from '../assets/officer_images/Cate.JPG';
-import VPExternalImg from '../assets/officer_images/Sam.JPG';
-import VPInternalImg from '../assets/officer_images/Valerie.JPG';
-import TreasurerImg from '../assets/officer_images/Suki.JPG';
-import SecretaryImg from '../assets/officer_images/Hannah.JPG';
-import EnvisionImg from '../assets/officer_images/coming_soon.png';
-import EDGEImg from '../assets/officer_images/Selena.JPG';
-import SWENextImg from '../assets/officer_images/Avery.JPG';
-import TechDevImg from '../assets/officer_images/Diane.JPG';
-import SocialImg from '../assets/officer_images/Marissa.JPG';
-import AcademicImg from '../assets/officer_images/Tanya.JPG';
-import PublicityImg from '../assets/officer_images/Melanie.JPG';
+import PresidentImg from '../assets/officer_images/Valerie.JPG';
+import VPExternalImg from '../assets/officer_images/coming_soon.png';
+import VPInternalImg from '../assets/officer_images/Melanie.JPG';
+import TreasurerImg from '../assets/officer_images/Hannah.JPG';
+import SecretaryImg from '../assets/officer_images/coming_soon.png';
+import EnvisionImg1 from '../assets/officer_images/Selena.JPG';
+import EnvisionImg2 from '../assets/officer_images/Avery.JPG';
+import EDGEImg1 from '../assets/officer_images/coming_soon.png';
+import EDGEImg2 from '../assets/officer_images/coming_soon.png';
+import SWENextImg from '../assets/officer_images/coming_soon.png';
+import TechDevImg from '../assets/officer_images/coming_soon.png';
+import SocialImg from '../assets/officer_images/coming_soon.png';
+import AcademicImg from '../assets/officer_images/coming_soon.png';
+import PublicityImg from '../assets/officer_images/coming_soon.png';
 
 const Officers = [
   {
-    name: 'Cate Readey',
+    name: 'Valerie Hope',
     position: 'President',
     image: PresidentImg,
     year: '4th Year',
-    major: 'Aerospace Engineering',
-    fact: 'I can do a headstand!',
-    college: 'Sixth',
-  },
-  {
-    name: 'Sam Ford',
-    position: 'VP External',
-    image: VPExternalImg,
-    year: '4th Year',
-    major: 'Mechanical Engineering',
-    fact: 'I have a cat!',
-    college: 'Warren',
-  },
-  {
-    name: 'Valerie Hope',
-    position: 'VP Internal',
-    image: VPInternalImg,
-    year: '3rd Year',
     major: 'Structural Engineering',
     fact: 'My favorite animal is a red panda!',
     college: 'Warren',
   },
   {
-    name: 'Suki Krishna',
-    position: 'Treasurer',
-    image: TreasurerImg,
-    year: '4th Year',
-    major: 'Bioengineering',
-    fact: 'I crocheted a backpack!',
+    name: 'Jordan Brown',
+    position: 'VP External',
+    image: VPExternalImg,
+    year: '2nd Year Transfer',
+    major: 'Aerospace Engineering',
+    fact: 'I love snowboarding!',
     college: 'Marshall',
   },
   {
-    name: 'Hannah Coates',
-    position: 'Secretary',
-    image: SecretaryImg,
-    year: '2nd Year',
+    name: 'Melanie Haro',
+    position: 'VP Internal',
+    image: VPInternalImg,
+    year: '3rd Year',
     major: 'Computer Science',
-    fact: 'I\'ve crocheted multiple blankets!',
+    fact: 'TBA',
     college: 'Seventh',
   },
   {
-    name: 'Coming Soon!',
-    position: 'Envision Chair',
-    image: EnvisionImg,
-    year: 'Coming Soon!',
-    major: 'Coming Soon!',
-    fact: 'Envision chair loves SWE!',
-    college: 'Coming Soon!',
+    name: 'Hannah Coates',
+    position: 'Treasurer',
+    image: TreasurerImg,
+    year: '3rd Year',
+    major: 'Computer Science',
+    fact: 'I love to crochet and knit!',
+    college: 'Seventh',
+  },
+  {
+    name: 'Yasmin Kabir',
+    position: 'Secretary',
+    image: SecretaryImg,
+    year: '2nd Year',
+    major: 'Math/CS',
+    fact: 'TBA',
+    college: 'TBA',
   },
   {
     name: 'Selena Cao',
-    position: 'EDGE Outreach Chair',
-    image: EDGEImg,
-    year: '2nd Year',
+    position: 'Envision Co-Chair',
+    image: EnvisionImg1,
+    year: '3rd Year',
     major: 'Bioengineering: Biotechnology',
     fact: 'I love pickles!',
     college: 'Muir',
   },
   {
     name: 'Avery Donaldson',
-    position: 'SWENext',
-    image: SWENextImg,
-    year: '2nd Year',
+    position: 'Envision Co-Chair',
+    image: EnvisionImg2,
+    year: 'Coming Soon!',
     major: 'Chemical Engineering',
     fact: 'I sewed my own prom dress!',
     college: 'Marshall',
   },
   {
-    name: 'Diane Bou-assi',
+    name: 'Sierra Lira',
+    position: 'EDGE Outreach Co-Chair',
+    image: EDGEImg1,
+    year: '3rd Year',
+    major: 'TBA',
+    fact: 'TBA',
+    college: 'TBA',
+  },
+  {
+    name: 'Sriya Paleti',
+    position: 'EDGE Outreach Co-Chair',
+    image: EDGEImg2,
+    year: '4th Year',
+    major: 'Bioengineering: Bioinformatics',
+    fact: 'I like painting!',
+    college: 'Warren',
+  },
+  {
+    name: 'Emi Takara',
+    position: 'SWENext',
+    image: SWENextImg,
+    year: '2nd Year',
+    major: 'TBA',
+    fact: 'TBA',
+    college: 'TBA',
+  },
+  {
+    name: 'Rachel McClow',
     position: 'Technical Development Chair',
     image: TechDevImg,
     year: '3rd Year',
-    major: 'Chemcial Engineering',
-    fact: 'I love hot sauce!',
-    college: 'Revelle',
+    major: 'TBA',
+    fact: 'TBA',
+    college: 'Warren',
   },
   {
-    name: 'Marissa Lee',
+    name: 'Roxana Pourfarzaneh',
     position: 'Social Chair',
     image: SocialImg,
-    year: '2nd Year',
-    major: 'Computer Science',
-    fact: 'I\'ve had a bee sting inside my ear!',
-    college: 'Muir',
+    year: 'TBA',
+    major: 'TBA',
+    fact: 'TBA',
+    college: 'Eighth',
   },
   {
-    name: 'Tanya Dwivedi',
+    name: 'Brooke Abselet',
     position: 'Academic Chair',
     image: AcademicImg,
-    year: '3rd Year',
-    major: 'Aerospace Engineering',
-    fact: 'I\'ve built a wind tunnel all by myself for an experiment!',
-    college: 'Revelle',
+    year: '2nd Year',
+    major: 'TBA',
+    fact: 'TBA',
+    college: 'TBA',
   },
   {
-    name: 'Melanie Haro-mendoza',
+    name: 'Ashley Campbell',
     position: 'Publicity Chair',
     image: PublicityImg,
-    year: '2nd Year',
-    major: 'Computer Science',
-    fact: 'Coming Soon!',
-    college: 'Seventh',
+    year: '4th Year',
+    major: 'Mechanical Engineering',
+    fact: 'I love soccer and baseball',
+    college: 'Sixth',
   },
 ];
 
